4f3257e383dade37ebdbcb9a0f401bd75670e4d2
Ofeliah Antoniette Flor M. Rubio <89459029+feliah1@users.noreply.github.com>
2021-09-22 22:16:10 +0800

    Add files via upload

---
+++ name.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+
+int main()
+{
+   //declaration of variables
+   int age = 18
+
+   //printing the variables
+   printf("My name is Ofeliah Antoniette Flor M Rubio");
+
+   printf("value of age is %d\n", age);
+
+   return 0;
+
+}
